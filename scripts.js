daadb944a5d16c295bea6629f770fae712104163
Jos <36787145+the-jos@users.noreply.github.com>
2022-06-09 01:05:19 +0200

    Updated from Publii

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ window.onload = function() {
   const customfooter = document.getElementById("customfooter");
   //const footerdiv = document.getElementsByClassName("footer")[0];
   //const actualfooter = document.getElementsByClassName("footer__copyright")[0];
-  const logo = document.querySelector("footer logo")[0];
+  const logo = document.querySelector("footer .logo")[0];
   const fragment = document.createDocumentFragment();
 
   fragment.appendChild(customfooter);

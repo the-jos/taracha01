83fbab56cc86ff504455a8cd91e0288bc817ca4b
Jos <36787145+the-jos@users.noreply.github.com>
2022-06-09 00:25:27 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 
 window.onload = function() {
   const customfooter = document.getElementById("customfooter");
-  const actualfooter = document.getElementsByClassName("footer");
+  const actualfooter = document.getElementsByClassName("footer")[0];
   const fragment = document.createDocumentFragment();
   fragment.appendChild(customfooter);
   actualfooter.appendChild(fragment);  

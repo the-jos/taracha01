07009049371fc42760d1a557c5d9e53720a0ab77
Jos <36787145+the-jos@users.noreply.github.com>
2022-06-13 20:38:39 +0200

    Updated from Publii

--- scripts.js
+++ scripts.js
@@ -2,15 +2,21 @@ window.onload = function() {
   const customfooter = document.getElementById("customfooter");
   const logo = document.querySelector("footer .logo");
   const fragment = document.createDocumentFragment();
+  const headerurl = document.querySelector("header > a");
 
   fragment.appendChild(customfooter);
   logo.parentNode.appendChild(fragment);
   logo.parentNode.insertBefore(customfooter, logo);
   editfooter(customfooter);
+  editheaderurl(headerurl)
 };
 
 function editfooter(customfooter) {
   customfooter.classList.add("flex-container")
-  customfooter.innerHTML = '<div class="flex-child f_left"><p>Flex Column 1</p></div>' +
+  customfooter.innerHTML = '<div class=""<div class="flex-child f_left"><p>Flex Column 1</p></div>' +
     '<div class="flex-child f_right"><p>Flex Column 2</p></div>';
+}
+
+function editheaderurl(headerurl) {
+	headerurl.innerHTML =+ '<p>test</p>'
 }

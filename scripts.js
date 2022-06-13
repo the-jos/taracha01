cf5280ef455ea77ed10098a6d9b6f3d2f3c8c66a
Jos <36787145+the-jos@users.noreply.github.com>
2022-06-13 20:46:21 +0200

    Updated from Publii

--- scripts.js
+++ scripts.js
@@ -18,5 +18,5 @@ function editfooter(customfooter) {
 }
 
 function editheaderurl(headerurl) {
-	headerurl.innerHTML += '<p>test</p>'
+	headerurl.innerHTML += '<img src="https://the-jos.github.io/taracha01/media/files/tar_banner_renovation02-small.jpg" alt="Taracha">'
 }

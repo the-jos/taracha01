569ed0802f05bf8b43a908ef06b96ece58b9e460
Jos <36787145+the-jos@users.noreply.github.com>
2022-06-14 23:20:37 +0200

    Updated from Publii

--- scripts.js
+++ scripts.js
@@ -13,11 +13,21 @@ window.onload = function() {
 
 function editfooter(customfooter) {
   customfooter.classList.add("flex-container")
-  customfooter.innerHTML = '<div class="flex-child f_left"><p>Flex Column 1</p></div>' +
-    '<div class="flex-child f_right"><p>Flex Column 2</p></div>';
+  customfooter.innerHTML = '<div class="flex-child f_left"><h2 id="contact">CONTACT ME</h2>' +
+  
+  '0518.927.234</address></div>' +
+  '<div class="flex-child f_right">' +
+  '<address><h2>TARACHA ZBIGNIEW BVBA</h2><br>' +
+  'Molenbaan 86<br>' +
+  '2900 Schoten<br>' +
+  '<a href="mailto:info@zbtaracha.pl">info@zbtaracha.pl<a><br>' +
+  '<a href="tel:+32499003355">+32 499 00 33 55</a><br></div>';
 };
 
 function editheaderurl(headerurl) {
 	headerurl.innerHTML += '<img src="https://the-jos.github.io/taracha01/media/files/tar_banner_renovation02-small.jpg" alt="Taracha">';
 	//headerurl.parentElement.innerHTML += '<div class="fill"></div>';
 };
+
+Molenbaan 86
+2900 Schoten
